691e8f765cbafa4d32d75b57b01692d54a846034
christopheradolphe <ckwadolphe@gmail.com>
2024-11-21 23:46:49 -0500

    Compute Jacobian for IBVS controller

--- templates/ibvs_jacobian.py
+++ templates/ibvs_jacobian.py
@@ -20,9 +20,34 @@ def ibvs_jacobian(K, pt, z):
     J  - 2x6 np.array, Jacobian. The matrix must contain float64 values.
     """
 
-    #--- FILL ME IN ---
+    # Extract focal and principal points from calibration matrix
+    f = K[0,0]
+    cx = K[0,2]
+    cy = K[1,2]
 
-    #------------------
+    # Extract image plane point coordinates
+    u, v = pt
+
+    # Calculate pixel coordiantes relative to principal point
+    u_bar = u - cx
+    v_bar = v - cy
+
+    # Initialize Jacobian to be all zeros
+    J = np.zeros((2,6))
+
+    # Populate Jacobian with non-zero values as per 2023 Corke Text (Equation 15.6)
+    J[0,0] = -f/z
+    J[0,2] = u_bar/z
+    J[0,3] = u_bar*v_bar / f
+    J[0,4] = -(f**2 + u_bar**2)/f
+    J[0,5] = v_bar
+
+    J[1,1] = -f/z
+    J[1,2] = v_bar/z
+    J[1,3] = (f**2+v_bar**2)/f
+    J[1,4] = -(u_bar*v_bar)/f
+    J[1,5] = -u_bar
+
 
     correct = isinstance(J, np.ndarray) and \
         J.dtype == np.float64 and J.shape == (2, 6)
